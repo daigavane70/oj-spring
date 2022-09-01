5fbe0661a5ef128238a34941f97c24b7c323633a
Rohit <rohitjames89@gmail.com>
2022-09-01 22:04:09 +0530

    dump added

--- common/src/main/java/com/sprint/common/GetContestStatus.java
+++
@@ -1,4 +0,0 @@
-package com.sprint.common;
-
-public class GetContestStatus {
-}
--- repository/src/main/resources/db/dump1.sql
+++ repository/src/main/resources/db/dump1.sql
@@ -27,9 +27,9 @@ DROP TABLE IF EXISTS `contests`;
 CREATE TABLE `contests` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
-  `start_time` datetime DEFAULT NULL,
-  `end_time` datetime DEFAULT NULL,
-  `total_questions` int DEFAULT NULL,
+  `startTime` datetime DEFAULT NULL,
+  `endTime` datetime DEFAULT NULL,
+  `totalQuestions` int DEFAULT NULL,
   `description` text,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -43,6 +43,34 @@ LOCK TABLES `contests` WRITE;
 /*!40000 ALTER TABLE `contests` DISABLE KEYS */;
 INSERT INTO `contests` VALUES (1,'Contest1','2022-09-01 13:18:17','2022-09-01 20:18:17',5,'This is the first ever contest you can ever see or play');
 /*!40000 ALTER TABLE `contests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `participants`
+--
+
+DROP TABLE IF EXISTS `participants`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `participants` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `contest_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `contest_id` (`contest_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `participants_ibfk_1` FOREIGN KEY (`contest_id`) REFERENCES `contests` (`id`),
+  CONSTRAINT `participants_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `participants`
+--
+
+LOCK TABLES `participants` WRITE;
+/*!40000 ALTER TABLE `participants` DISABLE KEYS */;
+/*!40000 ALTER TABLE `participants` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -73,6 +101,70 @@ CREATE TABLE `question` (
 LOCK TABLES `question` WRITE;
 /*!40000 ALTER TABLE `question` DISABLE KEYS */;
 /*!40000 ALTER TABLE `question` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `submission`
+--
+
+DROP TABLE IF EXISTS `submission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `submission` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `participant_id` int DEFAULT NULL,
+  `question_id` int DEFAULT NULL,
+  `contest_id` int DEFAULT NULL,
+  `result` json DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `contest_id` (`contest_id`),
+  KEY `user_id` (`user_id`),
+  KEY `participant_id` (`participant_id`),
+  KEY `question_id` (`question_id`),
+  CONSTRAINT `submission_ibfk_1` FOREIGN KEY (`contest_id`) REFERENCES `contests` (`id`),
+  CONSTRAINT `submission_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `submission_ibfk_3` FOREIGN KEY (`participant_id`) REFERENCES `participants` (`id`),
+  CONSTRAINT `submission_ibfk_4` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `submission`
+--
+
+LOCK TABLES `submission` WRITE;
+/*!40000 ALTER TABLE `submission` DISABLE KEYS */;
+/*!40000 ALTER TABLE `submission` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `test_cases`
+--
+
+DROP TABLE IF EXISTS `test_cases`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `test_cases` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `question_id` int DEFAULT NULL,
+  `input` json DEFAULT NULL,
+  `output` json DEFAULT NULL,
+  `explaination` json DEFAULT NULL,
+  `points` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `question_id` (`question_id`),
+  CONSTRAINT `test_cases_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `test_cases`
+--
+
+LOCK TABLES `test_cases` WRITE;
+/*!40000 ALTER TABLE `test_cases` DISABLE KEYS */;
+/*!40000 ALTER TABLE `test_cases` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -110,4 +202,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-01 21:23:07
+-- Dump completed on 2022-09-01 22:02:49

a01cce05b414d20515e8671b3ea42c2568fbf94e
Vedant Daigavane <vedant.daigavane@rksv.in>
2022-09-01 16:13:39 +0530

    initial dump added

--- server/src/main/java/com/sprint/server/MainApplication.java
+++ server/src/main/java/com/sprint/server/MainApplication.java
@@ -3,7 +3,6 @@ package com.sprint.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
